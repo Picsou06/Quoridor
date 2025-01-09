054fa21f45152ece6d78e41783c08c167c6d3960
Picsou06 <evanninou@gmail.com>
2025-01-09 08:12:22 +0100

    script working zith color

--- build.cmd
+++ build.cmd
@@ -1,7 +1,14 @@
 @echo off
 set C_INCLUDE_PATH=include
 
+rem Set color to green
+color 0A
+
 echo Compiling...
 gcc src\*.c -o bin\main.exe -Llib -lncursesw
 echo Running...
+
+rem Reset color to default
+color
+
 bin\main.exe
--- build.sh
+++ build.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
-# Check if ncurses is installed
 if ! command -v tput &> /dev/null; then
-    echo "ncurses is not installed. Installing now..."
+    echo -e "\e[31mncurses is not installed. Installing now...\e[0m"
 
-    # Check if the OS is Linux or macOS
     if [[ "$OSTYPE" == "linux-gnu"* ]]; then
         sudo apt-get update
         sudo apt-get install -y libncurses-dev
     elif [[ "$OSTYPE" == "darwin"* ]]; then
         brew install ncurses
     else
-        echo "Unsupported OS. Please install ncurses manually."
+        echo -e "\e[31mUnsupported OS. Please install ncurses manually.\e[0m"
         exit 1
     fi
-else
-    echo "Compiling..."
-    gcc src/*.c bin/main -Llib -lncurses
-    echo "Compilation complete."
-    echo "Running..."
-    ./bin/main
-fi
+echo -e "\e[32mCompiling...\e[0m"
+gcc src/*.c bin/main -Llib -lncurses
+echo -e "\e[32mCompilation complete.\e[0m"
+echo -e "\e[32mRunning...\e[0m"
+./bin/main
